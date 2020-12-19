e2a526c1b9175d6d1e0ccdc356fb0f8490b1d474
EydenVillanueva <eydenv85@gmail.com>
2020-12-19 13:08:47 -0500

    Next layout configuration and add home page

---
+++ webapp/components/layout/layout.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Link from 'next/link'
+import Head from 'next/head'
+
+import styles from './layout.module.css';
+import utilStyles from '../../styles/utils.module.css';
+
+
+const name = "Eyden Villanueva";
+export const siteTitle = "File System";
+
+export default function Layout({children, home}) {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <link rel="icon" href="/favicon.ico"/>
+                <meta
+                    name="description"
+                    content="Manage all related to File registration"
+                />
+                <meta
+                    property="og:image"
+                    content={`https://og-image.now.sh/${encodeURI(
+                        siteTitle
+                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+                />
+                <meta name="og:title" content={siteTitle}/>
+                <meta name="twitter:card" content="summary_large_image"/>
+            </Head>
+            <header className={styles.header}>
+                <>
+                    <h2 className={utilStyles.headingLg}>
+                        <a
+                            className={utilStyles.colorInherit}
+                            href="https://github.com/EydenVillanueva"
+                            target="_blank">
+                            Made by: {name}
+                        </a>
+                    </h2>
+                </>
+            </header>
+            <main className={styles.main}>
+                {children}
+            </main>
+            {!home && (
+                <div className={styles.backToHome}>
+                    <Link href="/">
+                        <a>← Back to home</a>
+                    </Link>
+                </div>
+            )}
+        </div>
+    );
+}
--- webapp/pages/index.js
+++ webapp/pages/index.js
@@ -1,209 +1,153 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Link from 'next/link';
+import Layout, {siteTitle} from '../components/layout/layout';
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <Layout home>
+            <Head>
+                <title>{siteTitle}</title>
+            </Head>
+            <h1 className="title">
+                Welcome to the File System Registration!
+            </h1>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+            <p className="description">
+                Get started by adding a new File
             </p>
-          </a>
-        </div>
-      </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
+            <div className="grid">
+                <Link href="files/register">
+                    <a href="" className="card">
+                        <h3>Add a new File &rarr;</h3>
+                        <p>Register a new File with the patient info, allergies and more...</p>
+                    </a>
+                </Link>
+                <a href="" className="card">
+                    <h3>Find File by Id &rarr;</h3>
+                    <p>Find any File stored to see its details...</p>
+                </a>
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+                <a
+                    href=""
+                    className="card"
+                >
+                    <h3>List of all Files &rarr;</h3>
+                    <p>See all the files displayed....</p>
+                </a>
+            </div>
+            <style jsx>{`                    
+              footer {
+                width: 100%;
+                height: 100px;
+                border-top: 1px solid #eaeaea;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+              }
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+              footer img {
+                margin-left: 0.5rem;
+              }
 
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+              footer a {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+              }
 
-        footer img {
-          margin-left: 0.5rem;
-        }
+              a {
+                color: inherit;
+                text-decoration: none;
+              }
 
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+              .title a {
+                color: #0070f3;
+                text-decoration: none;
+              }
 
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
+              .title a:hover,
+              .title a:focus,
+              .title a:active {
+                text-decoration: underline;
+              }
 
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
+              .title {
+                margin: 0;
+                line-height: 1.15;
+                font-size: 4rem;
+              }
 
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
+              .title,
+              .description {
+                text-align: center;
+              }
 
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
+              .description {
+                line-height: 1.5;
+                font-size: 1.5rem;
+              }
 
-        .title,
-        .description {
-          text-align: center;
-        }
+              code {
+                background: #fafafa;
+                border-radius: 5px;
+                padding: 0.75rem;
+                font-size: 1.1rem;
+                font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+                DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+              }
 
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
+              .grid {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                flex-wrap: wrap;
 
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
+                max-width: 800px;
+                margin-top: 3rem;
+              }
 
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
+              .card {
+                margin: 1rem;
+                flex-basis: 45%;
+                padding: 1.5rem;
+                text-align: left;
+                color: inherit;
+                text-decoration: none;
+                border: 1px solid #eaeaea;
+                border-radius: 10px;
+                transition: color 0.15s ease, border-color 0.15s ease;
+              }
 
-          max-width: 800px;
-          margin-top: 3rem;
-        }
+              .card:hover,
+              .card:focus,
+              .card:active {
+                color: #0070f3;
+                border-color: #0070f3;
+              }
 
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
+              .card h3 {
+                margin: 0 0 1rem 0;
+                font-size: 1.5rem;
+              }
 
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
+              .card p {
+                margin: 0;
+                font-size: 1.25rem;
+                line-height: 1.5;
+              }
 
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
+              .logo {
+                height: 1em;
+              }
 
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+              @media (max-width: 600px) {
+                .grid {
+                  width: 100%;
+                  flex-direction: column;
+                }
+              }
+            `}</style>
+        </Layout>
+    )
 }
